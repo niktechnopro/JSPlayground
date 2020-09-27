0b37ec62582bdf8f6f371ec4ed2dc2e1438c9a88
me <me>
2020-09-26 20:30:17 -0400

    updated with longer list

--- mergesort.js
+++ mergesort.js
@@ -1,4 +1,6 @@
-let test_array = [2,4,1,6,8,5,3,7];//unsorted
+// let test_array = [2,4,1,6,8,5,3,7];//unsorted
+
+let test_array = [31, 27, 28, 42, 13, 8, 11, 30, 17, 41, 15, 43, 1, 36, 9, 16, 20, 35, 48, 37, 7, 26, 34, 21, 22, 6, 29, 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 50, 46, 25, 18, 33, 47, 4, 45, 39, 23, 2];
 //create merge function - merging already sorted arrays
 function merge(l, r){//where l,r left and right halves
   let sorted = [];
@@ -43,3 +45,4 @@ function mergeSort(arr){
 }
 
 console.log(mergeSort(test_array));
+
