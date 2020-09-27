3f72bb258eb7e58e6eb928d37dfbc5210edf8a95
me <me>
2020-09-26 20:27:43 -0400

    merge sort completed

--- mergesort.js
+++ mergesort.js
@@ -1,48 +1,45 @@
-//this is divide and conquer algorithm
 let test_array = [2,4,1,6,8,5,3,7];//unsorted
-
-console.log(test_array)
-//split given array into 2 halves
-let halving = test_array.length/2;
-let left = test_array.slice(0, halving);
-let right = test_array.slice(halving);
-console.log(left, right);
-
 //create merge function - merging already sorted arrays
-function merge(l, r, array){
+function merge(l, r){//where l,r left and right halves
+  let sorted = [];
   //number of elements
   let lN = l.length;
   let rN = r.length;
   let i = 0, j = 0, k = 0; //simply initialization
   while(i < lN && j < rN){
     if(l[i] <= r[j]){
-      console.log("here", l[i])
-      array[k] = l[i];
+      sorted[k] = l[i];
       i++;
     }
     else{//case for l[i] > r[j]
-      console.log("there")
-      array[k] = r[j];
+      sorted[k] = r[j];
       j++;
     }
     k++;
-    console.log(array);
   }
 
   //copy remaining elements of left subarray, if any
   while(i < lN){
-    array[k] = l[i];
+    sorted[k] = l[i];
     i++;
     k++;
   }
 
   //copy remaining elements of right subarray, if any
   while(j < rN){
-    array[k] = r[j];
+    sorted[k] = r[j];
     j++;
     k++;
   } 
-  return array;
+  return sorted;
 }
 
-merge(left, right, test_array);
+function mergeSort(arr){
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2),
+      left = mergeSort(arr.slice(0, mid)),
+      right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+console.log(mergeSort(test_array));
