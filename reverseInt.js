1b662dae48092ee61985db9f09a85315fd701f51
me <me>
2020-10-22 20:10:42 -0400

    updated

---
+++ reverseInt.js
@@ -0,0 +1,30 @@
+// function reverseInt(n){
+//   //convert number to string, so we can use methods
+//   //1.check the sign negative or positive
+//   let sign = Math.sign(n);//we'll multiply on this
+//   //2.check if ends with 0 and if yes, remove it
+//   let newStr = n.toString();
+
+//   while(newStr.endsWith("0")){
+//     newStr = newStr.slice(0,-1);//slice method does not mutate original string
+//   }
+//   let result = "";
+//   for(let v of n.toString()){
+//     if(!Number.isNaN(parseInt(v))){
+//       result = v + result;
+//     }
+//   }
+//   return parseInt(result) * sign;
+// }
+
+//second way
+function reverseInt(n){
+  const reversed = n.toString().split("").reverse().join("");
+  if(n < 0){
+    return parseInt(reversed) * -1;
+  }
+  return parseInt(reversed);
+}
+
+// reverseInt(1290000);
+reverseInt(-109000000)
