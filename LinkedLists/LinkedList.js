f23478e4c7c93ac17525a08966f7ece9fc01a0bb
Nick <niktechnopro@gmail.com>
2021-05-08 08:43:17 -0400

    updated

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -23,26 +23,38 @@ class LinkedList{
   }
 
   size() {
-    let count = 0; 
-    console.log("this.head", this.head);
+    let count = 0;
     let node = this.head;
     while (node) {
         count++;
-        node = node.next
-        console.log("node: ", node)
+        node = node.next;
     }
     return count;
+  }
+
+  getFirst(){//this.head points to very first node of our linked list
+    return this.head;
   }
 }
 
 const nodeOne = new Node(5);
+// console.log("node: ", nodeOne);
 const list = new LinkedList();
+console.log("list before insertion: ", list);
 
+//insert element into list
+list.insertFirst(5);
+console.log("after insertion: ", list);
 
-console.log(nodeOne);
-list.head = nodeOne;
-console.log(list);
 list.insertFirst(7);
+console.log("after insertion: ", list);
+
 list.insertFirst(2);
-console.log(list);
+console.log("after insertion: ", list);
+
+
+//counting the size
 console.log(list.size());
+
+//getFirst - returns node that head is pointing to
+console.log(list.getFirst());
