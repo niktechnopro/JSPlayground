e7d69d9a98ad3fd051dab7a559d5b81da6e148af
Nick <niktechnopro@gmail.com>
2021-05-08 11:26:05 -0400

    clear method added to linked list

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -35,6 +35,21 @@ class LinkedList{
   getFirst(){//this.head points to very first node of our linked list
     return this.head;
   }
+
+  getLast(){//this should return the last node
+    if(this.head){//if not null - we have a Node
+      let node = this.head;
+      while (node && node.next) {
+          node = node.next;
+      }
+      return node;
+    }
+    return null;
+  }
+
+  clear(){//the head does not point to first node;
+    this.head = null;
+  }
 }
 
 const nodeOne = new Node(5);
@@ -57,4 +72,10 @@ console.log("after insertion: ", list);
 console.log(list.size());
 
 //getFirst - returns node that head is pointing to
-console.log(list.getFirst());
+console.log("first node: ", list.getFirst());
+
+//getLast - should return the last node
+console.log("last node: ", list.getLast());
+
+list.clear();//to empty the list
+console.log("clear? ", list.size());
