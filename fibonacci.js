039ec22910a6fe9b473fc89bc3119744e4784ec8
Nick <niktechnopro@gmail.com>
2021-05-23 11:41:35 -0400

    momoized version added

--- fibonacci.js
+++ fibonacci.js
@@ -42,3 +42,46 @@ fibonacci(2);
 // }
 
 // fibonacci(5);
+
+
+//another way - just a variation
+// function fibonacci(n){//time complexity O(n)
+//   let result = [];
+//   for(let i = 0; i <= n; i++){
+//     if(i < 2){
+//       result.push(i);
+//     }
+//     else{
+//       result.push(result[i-2]+result[i-1]);
+//     }
+//   }
+//   return result[n];
+// }
+
+// fibonacci(2);
+
+
+//memoizer
+function memoize(fn){
+  let cache = {}; //record of all the previous calls to this function
+  return function(...args){
+    if(cache[args]){
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  } 
+}
+
+//recursive
+function slowFib(n){//time complexity O(2^n) - exponential runtime. For each additional element of collection - the processing power required doubles;
+  if(n < 2){
+    return n;
+  }
+  return slowFib(n-1) + slowFib(n-2);
+}
+
+const fastFib = memoize(slowFib)
+
+console.log(fastFib(7))
